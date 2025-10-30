00d20bcf5cfe769ddfb65d7ccf7b2e4badfa1797
KolyaHulchuk <kola.gulchuk@gmail.com>
2025-10-30 18:33:09 +0000

    made the code cleaner by adding functions and also started developing pages for popular movies, TV series, etc.

--- movies/urls.py
+++ movies/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('', views.home, name='movies-home' ),  # це створення шляху
     path('movies/<int:pk>/', MoviesDetailView.as_view(), name='movies-detail'),
     path('about/', views.about, name='movies-about'),
-    path("search/", views.search, name="movies-search")
+    path("search/", views.search, name="movies-search"),
+    path("popular-movies/", views.popular_movies_view, name="popular-movies"),
+    path("tv-popular/", views.tv, name="popular-tv"),
+    path("top-rated/", views.top_rated_view, name="top-rated"),
+    path("now-playings/", views.now_playings_view, name="now-playings"),
+    path("upcoming/", views.upcoming_view, name="upcoming")
 ]
 
