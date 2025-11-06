19ae5236935981a67c04c418e89ffd2097403777
KolyaHulchuk <kola.gulchuk@gmail.com>
2025-11-06 12:26:19 +0000

    Add templates with extended movie list, shared base via include, and pagination

--- movies/tmdb_service.py
+++ movies/tmdb_service.py
@@ -1,141 +1,117 @@
 import requests
 import logging
+from datetime import datetime
 from django.conf import settings
 
-BASE_URL = "https://api.themoviedb.org/3"
+class TMDBClient:
+    BASE_URL = "https://api.themoviedb.org/3"
+    IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
-def get_movie_by_tmdb_id(tmdb_id):
-    url = f"{BASE_URL}/movie/{tmdb_id}"
-    params = {
-        "api_key": settings.TMDB_API_KEY
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        return response.json() # завантажує інформацію з json в python словник
-    return None
+    def __init__(self, api_key=None, languages=("en", "uk")):
+        self.api_key = api_key or settings.TMDB_API_KEY # Якщо api_key не передано, візьми settings.TMDB_API_KEY
+        self.languages = languages
 
-def get_tv_by_tmdb_id(tmdb_id):
-    url = f"{BASE_URL}/tv/{tmdb_id}"
-    params = {
-        "api_key": settings.TMDB_API_KEY
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        return response.json()
-    return None
-
-def add_poster_url(items):
-    for item in items:
-        path = item.get("poster_path")
-        item["poster_url"] = f"https://image.tmdb.org/t/p/w500{path}" if path else None
-
-    return items
-
-
-def add_extra_fields(items):
-    for item in items:
-        media_type = item.get("media_type", "movie")
-        item["title"] = item.get("title") if media_type == "movie" else item.get("name")
-        item["release_date"] = item.get("release_date") if media_type == "movie" else item.get("first_air_date")
-        item["rating"] = item.get("vote_average")
-    
-    return items
-
-def all_func_movie(endpoint: str, page: int, languages=("en", "uk")):
-    def fetch(language):
-        url = f"{BASE_URL}/{endpoint}"
-        params = {
-            "api_key": settings.TMDB_API_KEY,
-            "language": language,
-            "page": page
-        }
-
+    def _request(self, endpoint, params=None):
+        url = f"{self.BASE_URL}/{endpoint}"
+        params = params or {} # # якщо параметри не передані  створюю порожній словник
+        params["api_key"] = self.api_key # додаю до параметрів ключ API, щоб авторизувати запит до TMDB
         response = requests.get(url, params=params)
         if response.status_code == 200:
             return response.json() # — це метод, який перетворює JSON-відповідь з сервера на Python-словник.
         else:
-            logging.warning(f"Error API {response.status_code}")
-        return {"results": [], "page": 1, "total_pages": 1}
+            logging.warning(f"TMDB API error {response.status_code} for endpoint '{endpoint}")
+        return {} # тут пусто бо дані даля обробляюст і там вже будуть добавлений результ та сторінки
+
+
+    def get_movie_by_tmdb_id(self, tmdb_id):
+        return self._request(f"movie/{tmdb_id}")
+
+    def get_tv_by_tmdb_id(self, tmdb_id):
+        return self._request(f"tv/{tmdb_id}")
+        
+
+    def _type_items(self, items):
+        for item in items:
+            path = item.get("poster_path")
+            item["poster_url"] = f"https://image.tmdb.org/t/p/w500{path}" if path else None
+
+            media_type = item.get("media_type", "movie")
+            item["title"] = item.get("title") if media_type == "movie" else item.get("name")
+            item["release_date"] = item.get("release_date") if media_type == "movie" else item.get("first_air_date")
+            item["rating"] = item.get("vote_average")
+        
+        return items
     
-    all_results = []
-    base_data = None
-
-    for lang in languages:
-        data = fetch(lang)
-        if lang == 'en':
-            base_data = data
-        all_results.extend(data.get("results"))
-
-    uniq_results = []
-    seen_id = set()
-    for movie in all_results:
-        if movie["id"] not in seen_id:
-            seen_id.add(movie["id"])
-            uniq_results.append(movie)
-        
-    uniq_results = add_poster_url(uniq_results)
-    uniq_results = add_extra_fields(uniq_results)
-
-    return {
-        "results": uniq_results,
-        "page": base_data.get("page", 1),
-        "total_pages": base_data.get("total_pages", 1)
-    }
     
+    def get_list(self, endpoint, page=1):
+        data = self._request(endpoint, {"language": "en", "page": page})
+        if not data or "results" not in data:
+            return {
+                "results": [],
+                "page": page,
+                "total_pages": 1
+            }
 
+        uniq_results = []
+        seen_id = set()
 
-def popular_movies(page):
-    return all_func_movie("movie/popular", page)
-        
+        for result in data["results"]:
+            if result["id"] not in seen_id:
+                seen_id.add(result["id"])
+                uniq_results.append(result)
 
-def tv_popular(page):
-    return all_func_movie("tv/popular", page)
+        uniq_results = self._type_items(uniq_results)
 
-
-def movie_top_rated(page):
-    return all_func_movie("movie/top_rated", page)
-  
-
-def movie_now_playings(page):
-    return all_func_movie("movie/now_playing", page)
-      
-
-def movie_upcoming(page):
-    return all_func_movie("movie/upcoming", page)
-        
-
-def search_movies(query): # query — це рядок із того, що ввів користувач у форму пошуку 
-    def fetch(language):
-        url = f"{BASE_URL}/search/multi"
-        params = {
-            "api_key": settings.TMDB_API_KEY, 
-            "query": query,
-            "language": language
+        return {
+            "results": uniq_results,
+            "page": data.get("page", page),
+            "total_pages": min(data.get("total_pages", 1), 10)
         }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            return response.json().get("results", [])
-        else:
-            logging.warning(f"TMDB API error {response.status_code} for query '{query}' in '{language}'")
-        return []
-
-    results_uk = fetch("uk")
-    results_en = fetch("en")
-
-    seen_ids = set()
-    combined = []
-
-    for item in results_uk + results_en:
-        media_type = item.get("media_type")
-        if media_type not in ["movie", "tv"]:
-            continue # пропускаємо акторів та інше
-        if item["id"] not in seen_ids:
-            combined.append(item)
-            seen_ids.add(item["id"])
-
-    return combined
 
 
+   
+    def search_movies(self, query): # query — це рядок із того, що ввів користувач у форму пошуку 
+        seen_ids = set()
+        combined = []
 
+        for lang in self.languages:
+            data = self._request("search/multi", {"query": query, "language": lang})
+            for item in data.get("results", []):
+                media_type = item.get("media_type")
+                if media_type not in ["movie", "tv"]:
+                    continue # пропускаємо акторів та інше
+                if item["id"] not in seen_ids:
+                    combined.append(item)
+                    seen_ids.add(item["id"])
 
+        return self._type_items(combined)
+
+    @staticmethod
+    def get_release_date(details, media_type):
+        raw_date = (
+            details.get("release_date") if media_type == "movie" 
+            else details.get("first_air_date")
+        )
+
+        try:
+            date_obj = datetime.strptime(raw_date, "%Y-%m-%d")
+            return date_obj.strftime("%d.%m.%Y")
+        except (TypeError, ValueError):
+            return "Uknown"
+        
+
+    def enrich_item(self, item, media_type):
+            details = (
+                self.get_movie_by_tmdb_id(item["id"])
+                if media_type == "movie"
+                else self.get_tv_by_tmdb_id(item["id"])
+            )
+
+            item["release_date"] = self.get_release_date(details, media_type)
+            item["tmdb_rating"] = details.get("vote_average")
+            item["genres"] = [ genre["name"] for genre in details.get("genres", [])]
+            return item
+    
+    def enrich_items(self, items, media_type):
+        return [self.enrich_item(item, media_type) for item in items]
 # шукає в цьому словнику ключ "results". Якщо він є  повертає значення (тобто список фільмів). Якщо його немає  повертає порожній список [] 
