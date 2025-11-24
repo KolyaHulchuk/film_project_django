5b08fac3ff6af910cff0c7e1e301b06fa7e940fc
KolyaHulchuk <kola.gulchuk@gmail.com>
2025-11-24 10:51:42 +0000

    Update urls and add password reset template

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout' ),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_reset"),
     path('profile/', user_views.profile, name='profile')
 ]
 
