85f669c70b0e22082a83c436935462c776465c2e
KolyaHulchuk <kola.gulchuk@gmail.com>
2026-05-29 12:45:33 +0000

    Fix image existence check

--- users/models.py
+++ users/models.py
@@ -19,10 +19,10 @@ class Profile(models.Model):
         if os.path.exists(self.image.path):
             img = Image.open(self.image.path)
         
-        if img.height > 300 and img.width > 300:
-            outup_size = (300 , 300)
-            img.thumbnail(outup_size)
-            img.save(self.image.path)
+            if img.height > 300 or img.width > 300:
+                outup_size = (300 , 300)
+                img.thumbnail(outup_size)
+                img.save(self.image.path)
 
 
 class Watchlist(models.Model):
